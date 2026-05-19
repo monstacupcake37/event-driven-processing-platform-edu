ea37459f0628b0abe29606e01241140c263daac3
monstacupcake37 <eogurtsovsky@gmail.com>
2026-05-19 18:10:40 +0300

    Add files via upload

---
+++ notification-service/src/test/java/org/mc17171/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mc17171.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
