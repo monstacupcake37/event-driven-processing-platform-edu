aaf76b2981ae8515efbff1ee150c75244ba66f2d
monstacupcake37 <memerage88@gmail.com>
2026-05-22 15:26:24 +0300

    Test of OrderServiceApplication

--- order-service/src/main/java/org/mc17171/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/org/mc17171/orderservice/OrderServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
